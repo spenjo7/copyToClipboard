977a411755b07559c90d4b09693563019624ac0e
spenjo7 <palm.pixi.plus@gmail.com>
2021-01-28 00:45:26 -0500

    Adding the createCopyButton( text, str, target_element, prepend ) which allows the easy creation and populating of a button which copies specific text.

--- easyClip.js
+++ easyClip.js
@@ -7,3 +7,18 @@ const copyToClipboard = ( text ) => {
       document.body.removeChild(input)
       return result
 }
+
+const createCopyButton = ( btn_txt = 'Copy', copy_str = 'nothing to copy', target_element = document.body, prepend = false ) => {
+  const btn = document.createElement('button')
+  btn.textContent = btn_txt
+  btn.addEventListener('click', (e) => { 
+    e.preventDefault()
+    copyToClipboard(copy_str) 
+  })
+
+  if(prepend){
+    target_element.prepend(btn)
+  } else{
+    target_element.appendChild(btn)
+  }
+}
