7d6fba963d113c34586f375427605487b168e01a
spenjo7 <palm.pixi.plus@gmail.com>
2021-01-28 01:33:40 -0500

    Changing the createButton() function. Now the last variable is an options object; this allows for the prepend boolean as well as id and class attribute for the button

--- easyClip.js
+++ easyClip.js
@@ -8,13 +8,19 @@ const copyToClipboard = ( text ) => {
       return result
 }
 
-const createCopyButton = ( btn_txt = 'Copy', copy_str = 'nothing to copy', target_element = document.body, prepend = false ) => {
+const createCopyButton = ( btn_txt = 'Copy', copy_str = 'nothing to copy', target_element = document.body, opts = {} ) => {
+  // Options: prepend <boolean>  id <string>  classes <string>
+
   const btn = document.createElement('button')
   btn.textContent = btn_txt
   btn.addEventListener('click', (e) => { 
     e.preventDefault()
     copyToClipboard(copy_str) 
   })
+
+  const { prepend, id, classes } = opts
+  if(id){ btn.id = id }
+  if (classes){ btn.classList = classes }
 
   if(prepend){
     target_element.prepend(btn)
