7a20b400c8243a3fa9c1eef0ac20a6d77750cc76
spenjo7 <palm.pixi.plus@gmail.com>
2021-02-16 01:48:05 -0500

    Added a CopyButton class which expands on the createCopyButton functionality

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # copyToClipboard
 A simple Code Snippet to Copy from JavaScript to the Clipboard ( I use this all the time! )
+- Also includes a function to generate Buttons which use the script 
+- Now includes a class to creates a button copies the trackd innertext/value of a source DOM Element
+	-- Or the text to copy can be externally updated 
+
+## Useage:
+copyToClipboard( textString ) // Use as a function in an event listener
+createCopyButton( textString, buttonLabel, DOMtargetElement, { prepend, id, classList })
+new CopyButton( storedText, buttonLabel, DOMtargetElement, { prepend, id, classList })
+	.update(text) // externally updates the text to copy
+	.setSource(sourceDomElement) // designates a DOM Element to be the source of the copyable text;
+		-- *Experimental* Either the element's innerText or value will be copied 
+
+## Updated on 2021-02-15 
+-- copyToClipboard Now includes include navigator.clipboard API's .writeText() method
+-- Added the CopyButton class, which is similar to createCopyButton but it also has a .setSource()
--- easyClip.js
+++ easyClip.js
@@ -1,30 +1,101 @@
 const copyToClipboard = ( text ) => {
-      let input = document.createElement('textarea')
-      input.innerHTML = text
-      document.body.appendChild(input)
-      input.select()
-      let result = document.execCommand('copy')
-      document.body.removeChild(input)
-      return result
+	if(!navigator.clipboard){
+		const input=document.createElement('textarea')
+		input.innerText=text
+		document.body.appendChild(input)
+		input.select()
+		const result=document.execCommand('copy')
+		document.body.removeChild(input)
+		return result 
+	} else {
+		navigator.clipboard.writeText(text) 
+	}
 }
 
-const createCopyButton = ( btn_txt = 'Copy', copy_str = 'nothing to copy', target_element = document.body, opts = {} ) => {
-  // Options: prepend <boolean>  id <string>  classes <string>
 
-  const btn = document.createElement('button')
-  btn.textContent = btn_txt
-  btn.addEventListener('click', (e) => { 
-    e.preventDefault()
-    copyToClipboard(copy_str) 
-  })
+const createCopyButton = ( text='', buttonLabel='Copy', DOMtargetElement=document.body, opts = {} ) => {
+	// Options: prepend <boolean>  id <string>  classes <string>
 
-  const { prepend, id, classes } = opts
-  if(id){ btn.id = id }
-  if (classes){ btn.classList = classes }
+	const btn = document.createElement('button')
+	btn.textContent = buttonLabel
+	btn.addEventListener('click', (e) => { 
+		e.preventDefault()
+		copyToClipboard(text) 
+	})
 
-  if(prepend){
-    target_element.prepend(btn)
-  } else{
-    target_element.appendChild(btn)
-  }
+	const { classList, id, prepend } = opts
+	if(id){ btn.id = id }
+	if (classList){ btn.classList.add(classList) }
+
+	if(prepend){
+		DOMtargetElement.prepend(btn)
+	} else{
+		DOMtargetElement.appendChild(btn)
+	}
+}
+
+
+class CopyButton{
+	constructor( storedText, buttonLabel, DOMtargetElement, opts){
+
+		this.btn=document.createElement('button')
+		this.storedText=storedText?? ''
+		this.buttonLabel=buttonLabel?? 'Copy'
+		this.DOMtargetElement=DOMtargetElement?? document.body
+		this.prepButton(opts?? {})
+	}
+
+	setSource(sourceDomElement){
+		this.contentSource=sourceDomElement?? null // clears on empty
+		console.log(sourceDomElement)
+	}
+
+	update(newText = null ){
+		this.storedText=newText
+	}
+
+	verifyStoredText(){
+		if(this.contentSource){
+			const tag = this.contentSource.tagName
+			const useValue = ["TEXTAREA"].find( el => el === tag )
+			const currentText = useValue? this.contentSource.value 
+				: this.contentSource.innerText
+			this.update(currentText)
+		}
+	}
+
+	copyToClipboard(){
+		this.verifyStoredText()
+		
+		if(!navigator.clipboard){
+			const input=document.createElement('textarea')
+			input.innerText=this.storedText
+			document.body.appendChild(input)
+			input.select()
+			const result=document.execCommand('copy')
+			document.body.removeChild(input)
+			return result 
+		} else {
+			navigator.clipboard.writeText(this.storedText) 
+		}
+	}
+
+	prepButton(options){
+		const { classList, id, prepend } = options
+		
+		if(id){ this.btn.id = id }
+		if (classList){ this.btn.classList.add(classList) }
+
+		this.btn.textContent=this.buttonLabel
+		this.btn.addEventListener('click', (e) => { 
+			e.preventDefault()
+			this.copyToClipboard() 
+		})
+
+		if(prepend){
+			this.DOMtargetElement.prepend(this.btn)
+		} else{
+			this.DOMtargetElement.appendChild(this.btn)
+		}
+	}
 }
